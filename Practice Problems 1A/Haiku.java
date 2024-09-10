b81a6f5bec4d1fbd8fcad64df662932f81e1360d
Evital Heyl <evitalheyl@gmail.com>
2024-09-10 12:33:48 -0700

    Add files via upload

---
+++ Practice Problems 1A/Haiku.java
@@ -0,0 +1,12 @@
+/**
+ * Haiku
+ * Evital Heyl
+ */
+
+public class Haiku {
+    public static void main (String[] args) {
+    System.out.println ("The Calling Monsters");
+    System.out.println ("I Heard Them Shouting My Name");
+    System.out.println ("I Must Follow Them");
+    }
+}
