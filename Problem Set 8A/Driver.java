921f50f69310e8e4961daa93bca85a51f5027015
Evital Heyl <evitalheyl@gmail.com>
2024-12-06 14:57:34 -0800

    Add files via upload

--- Problem Set 8A/Driver.java
+++ Problem Set 8A/Driver.java
@@ -7,21 +7,39 @@
 import java.util.Scanner;
 
 public class Driver {
-  public static void main (String[] args) {
-       Scanner input = new Scanner(System.in);
-       Grid myGrid = new Grid();
-       
-       boolean won = false;
-       int roundNum = 1;
-       
-       while (!won) {
-           System.out.println("Round " + roundNum);
-           myGrid.draw();
-           System.out.print("X, make your move (row, col): ");
-           String move = input.nextLine();
-           
-       }
-    }  
-    
+    public static void main (String[] args) {   
 
+        Scanner input = new Scanner(System.in);
+        boolean Continue = true;
+        while(Continue) {
+            Grid myGrid = new Grid();
+
+            int roundNum = 1;
+            String player = "X";
+
+            while (!myGrid.won()) {
+                if(roundNum%2 != 0) player = "X";
+                else player = "O";
+
+                System.out.println("Round " + roundNum);  
+                myGrid.draw();
+
+                System.out.print(player + ", make your move (row, col): ");
+                String move = input.nextLine();
+                myGrid.add(myGrid.getRow(move), myGrid.getColumn(move), player);
+
+
+                roundNum++;
+            }
+
+            System.out.println("YOU WON");
+            myGrid.draw();
+            System.out.print("Play Again? Y/N ");
+            String again = input.nextLine();
+            if (again.equals("Y")) Continue = true;
+            if (again.equals("N")) Continue = false;
+
+        }
+
+    }  
 }
--- Problem Set 8A/Grid.java
+++ Problem Set 8A/Grid.java
@@ -11,7 +11,7 @@ public class Grid {
       grid = new String[3][3]; 
       for (int row = 0; row < grid.length; row++) {
           for (int column = 0; column < grid[0].length; column++) {
-              grid[row][column] = "";
+              grid[row][column] = " ";
           }
       }
       
@@ -22,7 +22,7 @@ public class Grid {
       
       for (int row = 0; row < grid.length; row++) {
           for (int column = 0; column < grid[0].length; column++) {
-              System.out.print("[ " + grid[row][column] + " ]");
+              System.out.print("[" + grid[row][column] + "]");
           }
           System.out.println();
       }
@@ -33,9 +33,38 @@ public class Grid {
   }
   
   public static int getRow (String move) {
-      for (int i = 0; i < move.length(); i++) {
-          //if (move.charAt(i) != ' ') return 
-      }
-      return 0;
+    move = move.trim();
+    return Integer.parseInt(move.substring(0, 1));
+
   }
+  
+   public static int getColumn (String move) {
+    move = move.trim();
+    return Integer.parseInt(move.substring(move.length()-1));        
+  }
+  
+  public boolean won () {
+      for (int row = 0; row < grid.length; row++) {
+          String current = grid[row][0];
+          for (int column = 1; column < grid[0].length - 1; column++) {
+              if(grid[row][column].equals(current) && grid[row][column + 1].equals(current) && !current.equals(" ")) return true;        
+          }
+      }
+      
+      for (int column = 0; column < grid[0].length; column++) {
+          String current = grid[0][column];
+          for (int row = 1; row < grid.length - 1; row++) {
+              if(grid[row][column].equals(current) && grid[row + 1][column].equals(current) && !current.equals(" " )) return true;          
+          }
+      }
+      
+      if (!grid[0][0].equals(" ") && grid[0][0].equals(grid[1][1]) && grid[1][1].equals(grid[2][2])) return true;
+      
+      if (!grid[2][0].equals(" ") && grid[2][0].equals(grid[1][1]) && grid[1][1].equals(grid[0][2])) return true;
+        
+      return false;
+      
+      
+  }
+  
 }
