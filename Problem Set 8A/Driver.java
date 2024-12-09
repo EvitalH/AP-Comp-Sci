d01306ef7fa1cd362c533a905bce87641cf0c297
Evital Heyl <evitalheyl@gmail.com>
2024-12-09 15:01:14 -0800

    Add files via upload

--- Problem Set 8A/Driver.java
+++ Problem Set 8A/Driver.java
@@ -7,39 +7,67 @@
 import java.util.Scanner;
 
 public class Driver {
+    static boolean playing = true;
+    static Grid myGrid = new Grid();
+    static Scanner input = new Scanner(System.in);
+    static int roundNum = 1;
+    static String player;
+
     public static void main (String[] args) {   
+        while(playing) {
+            game();
+            end();
+        }
+    }
 
-        Scanner input = new Scanner(System.in);
-        boolean Continue = true;
-        while(Continue) {
-            Grid myGrid = new Grid();
+    public static void game () {
+        myGrid.instantiate();
 
-            int roundNum = 1;
-            String player = "X";
+ 
 
-            while (!myGrid.won()) {
-                if(roundNum%2 != 0) player = "X";
-                else player = "O";
-
+        while (!myGrid.won()) {
+            if(roundNum%2 != 0) player = "X";
+            else player = "O";
+            
+            String move = "";
+            
+            do {
                 System.out.println("Round " + roundNum);  
                 myGrid.draw();
+                System.out.print(player + ", make your move (row,col): ");
+                move = input.nextLine();
+            } while(!myGrid.action(myGrid.getRow(move), myGrid.getColumn(move), player));
 
-                System.out.print(player + ", make your move (row, col): ");
-                String move = input.nextLine();
-                myGrid.add(myGrid.getRow(move), myGrid.getColumn(move), player);
 
+            roundNum++;
+            if(roundNum == 10) break;
+        }
+    }  
 
-                roundNum++;
+    public static void end () {
+        boolean correctAnswer = false;
+
+        while(!correctAnswer) {
+            if (roundNum == 10 && !myGrid.won()){
+                System.out.println("TIE GAME");
+            }else System.out.println("YOU WON PLAYER " + player);
+            
+            myGrid.draw();
+            System.out.print("Play Again? Y/N ");     
+            String again = input.nextLine();
+            
+            if (again.equals("Y")) {
+                playing = true;
+                correctAnswer = true;
+                roundNum = 1;
+                myGrid.instantiate();
+            }
+            if (again.equals("N")) {
+                playing = false;
+                correctAnswer = true;
             }
 
-            System.out.println("YOU WON");
-            myGrid.draw();
-            System.out.print("Play Again? Y/N ");
-            String again = input.nextLine();
-            if (again.equals("Y")) Continue = true;
-            if (again.equals("N")) Continue = false;
-
         }
-
-    }  
+    }
 }
+
--- Problem Set 8A/Grid.java
+++ Problem Set 8A/Grid.java
@@ -5,66 +5,105 @@
  * Problem Set 8A
  */
 public class Grid {
-     private String[][] grid;
-     
-    Grid (){
-      grid = new String[3][3]; 
-      for (int row = 0; row < grid.length; row++) {
-          for (int column = 0; column < grid[0].length; column++) {
-              grid[row][column] = " ";
-          }
-      }
-      
-    }
-    
-  public void draw () {
-      
-      
-      for (int row = 0; row < grid.length; row++) {
-          for (int column = 0; column < grid[0].length; column++) {
-              System.out.print("[" + grid[row][column] + "]");
-          }
-          System.out.println();
-      }
-  }
-  
-  public void add (int row, int column, String play) {
-      grid[row][column] = play;
-  }
-  
-  public static int getRow (String move) {
-    move = move.trim();
-    return Integer.parseInt(move.substring(0, 1));
+    private String[][] grid;
 
-  }
-  
-   public static int getColumn (String move) {
-    move = move.trim();
-    return Integer.parseInt(move.substring(move.length()-1));        
-  }
-  
-  public boolean won () {
-      for (int row = 0; row < grid.length; row++) {
-          String current = grid[row][0];
-          for (int column = 1; column < grid[0].length - 1; column++) {
-              if(grid[row][column].equals(current) && grid[row][column + 1].equals(current) && !current.equals(" ")) return true;        
-          }
-      }
-      
-      for (int column = 0; column < grid[0].length; column++) {
-          String current = grid[0][column];
-          for (int row = 1; row < grid.length - 1; row++) {
-              if(grid[row][column].equals(current) && grid[row + 1][column].equals(current) && !current.equals(" " )) return true;          
-          }
-      }
-      
-      if (!grid[0][0].equals(" ") && grid[0][0].equals(grid[1][1]) && grid[1][1].equals(grid[2][2])) return true;
-      
-      if (!grid[2][0].equals(" ") && grid[2][0].equals(grid[1][1]) && grid[1][1].equals(grid[0][2])) return true;
+    public void instantiate (){
+        grid = new String[3][3]; 
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[0].length; column++) {
+                grid[row][column] = " ";
+            }
+        }
+
+    }
+
+    public void draw () {
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[0].length; column++) {
+                System.out.print("[" + grid[row][column] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean action (int row, int column, String play) {
+        if(row < 0 || row > 2 || column < 0 || column > 2) return false; //is play in range?
         
-      return false;
-      
-      
-  }
-  
+        if (grid[row][column].equals(" ")) {  //has this space already been filled?
+            grid[row][column] = play;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static int getRow (String move) {
+        move = move.trim();
+        
+        int index = 0;
+        for (int i = 0; i < move.length(); i++) {
+            if (move.charAt(i) == ',') index = i;     
+        }
+
+        boolean numeric = true; //will check if the move is in the right format
+        
+        try {
+            Integer.parseInt(move.substring(0, index));
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)return Integer.parseInt(move.substring(0, index));
+        else return -1;
+
+    }
+
+    public static int getColumn (String move) {
+        move = move.trim();
+        int index = 0;
+        for (int i = 0; i < move.length(); i++) {
+            if (move.charAt(i) == ',') index = i;     
+        }
+
+        boolean numeric = true; //will check if the move is in the right format
+        try {
+            Integer.parseInt(move.substring(index + 1));
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)return Integer.parseInt(move.substring(index + 1));
+        else return -1;        
+    }
+
+    public boolean won () {
+        //Won a row
+        
+        for (int row = 0; row < grid.length; row++) {
+            String current = grid[row][0];
+            for (int column = 1; column < grid[0].length - 1; column++) {
+                if(grid[row][column].equals(current) && grid[row][column + 1].equals(current) && !current.equals(" ")) return true;        
+            }
+        }
+
+        //Won a column
+        
+        for (int column = 0; column < grid[0].length; column++) {
+            String current = grid[0][column];
+            for (int row = 1; row < grid.length - 1; row++) {
+                if(grid[row][column].equals(current) && grid[row + 1][column].equals(current) && !current.equals(" " )) return true;          
+            }
+        }
+
+        //Won a diagonal
+        
+        if (!grid[0][0].equals(" ") && grid[0][0].equals(grid[1][1]) && grid[1][1].equals(grid[2][2])) return true;
+
+        if (!grid[2][0].equals(" ") && grid[2][0].equals(grid[1][1]) && grid[1][1].equals(grid[0][2])) return true;
+
+        return false; //didn't win
+
+    }
 }
