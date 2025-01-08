bec7c05d20de906384185af23b976f6068e9dad0
Evital Heyl <evitalheyl@gmail.com>
2025-01-08 14:57:41 -0800

    Add files via upload

--- Problem Set 5A/Fraction.java
+++ Problem Set 5A/Fraction.java
@@ -11,13 +11,13 @@ public class Fraction {
     
     public Fraction () { //default
         numerator = 1;
-        denominator = 2;
+        denominator = 1;
     }
     
     public Fraction (int num, int den) { //custom
         numerator = num;
-        denominator = den;
-        checkZero(denominator);
+        setDenom(den);
+
         
     }
     
@@ -41,6 +41,7 @@ public class Fraction {
         if ( den == 0) throw new ArithmeticException ("you cannot divide by 0");
     }
     
+    //ACCESSOR METHODS
     public int getNum () {
         return numerator;
     }
@@ -49,16 +50,85 @@ public class Fraction {
         return denominator;
     }
     
+    public String toString () {
+        return numerator + "/" + denominator;
+    }
+    
     public double toDouble () {
         return (double)numerator/denominator;
     }
     
-    public Fraction multiply (Fraction fracOne, Fraction fracTwo) {
+    //MUTATOR METHODS
+    
+    public void reduce() {
+        int gcf = greatestCommonFactor(numerator, denominator);
+        numerator = numerator/gcf;
+        denominator = denominator/gcf;
+    }
+    
+    public void setNum (int n) {
+        checkZero(n);
+        numerator = n;
+    }
+    
+    public void setDenom (int n) {
+        checkZero(n);
+        denominator = n;
+    }
+    
+    //STATIC METHODS
+    
+    public static Fraction multiply (Fraction fracOne, Fraction fracTwo) {
         int Num = fracOne.numerator * fracTwo.numerator;
         int Denom = fracOne.denominator * fracTwo.denominator;
         Fraction myFraction = new Fraction(Num, Denom);
         return myFraction;
     }
+    
+    public static Fraction divide (Fraction fracOne, Fraction fracTwo) {
+        int Num = fracOne.numerator * fracTwo.denominator;
+        int Denom = fracOne.denominator * fracTwo.numerator;
+        Fraction myFraction = new Fraction(Num, Denom);
+        return myFraction;
+    }
+    
+    private static Fraction AddOrSubtract (Fraction fracOne, Fraction fracTwo, String change) {
+        int NumOne = fracOne.numerator * fracTwo.denominator;
+        int NumTwo = fracTwo.numerator * fracOne.denominator;
+        int Denom = fracOne.denominator * fracTwo.denominator;
+        
+        int Num;
+        if (change.equals("add")) Num = NumOne + NumTwo;
+        else Num = NumOne - NumTwo;
+        
+        Fraction myFraction = new Fraction(Num, Denom);
+        myFraction.reduce();
+        return myFraction;
+        
+    }
+    
+    
+    public static Fraction add (Fraction fracOne, Fraction fracTwo) {
+        return AddOrSubtract(fracOne, fracTwo, "add");
+    }
+    
+    public static Fraction subtract (Fraction fracOne, Fraction fracTwo) {
+        return AddOrSubtract(fracOne, fracTwo, "subtract");
+    }
+    
+    
+    //HELPER METHODS
+    
+    public static int greatestCommonFactor (int a, int b) {
+        
+        while(a != b) {
+            int largerNumber = Math.max(a, b);
+            if (largerNumber == a) a = a - b;
+            else if (largerNumber == b) b = b - a;               
+        }
+        return a;
+    }
+    
     
     
     
--- Problem Set 5A/FractionDriver.java
+++ Problem Set 5A/FractionDriver.java
@@ -5,7 +5,10 @@
  * Problem Set 5A
  */
 public class FractionDriver {
-    void main () {
-        
+    public static void main (String[] args) {
+        Fraction myFractionOne = new Fraction (1, 2);
+        Fraction myFractionTwo= new Fraction (1, 4);
+
+        System.out.println( Fraction.add(myFractionOne, myFractionTwo));
     }
 }
